16827964de9b52b0aab9c6bc31b0a567fa16dcb1
Edgardo Ramírez <soldiercrp@gmail.com>
2017-10-20 22:02:16 -0500

    ADD: .editorconfig, style file, courses.json and flow to show courses from .json file.

---
+++ scripts/app.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+  /*
+    Functions
+  */
+  populateCourses = function() {
+    let $coursesEl = $('#courses');
+
+    $.map(courses, function(course, i) {
+      let $list = $('<div id="'+course.slug+'" class="course web '+course.category+'">');
+      let $details = $('<details></details>');
+      let $summary = $('<summary>'+course.name+'</details>');
+
+      $details.append($summary);
+
+      $.map(course.links, function(link, i) {
+        let $link =  $('<a href="'+link.url+'" class="link" target="_blank">'+link.name+'</a>')
+        $details.append($link);
+      });
+
+      $list.append($details);
+      $coursesEl.append($list);
+    });
+  }
+
+
+  window.showCourse = function(type) {
+    $('.course').hide();
+    $('.' + type).show();
+    $('#menuItems *').removeClass('active');
+    $('#show_' + type).addClass('active');
+  }
+
+  window.showAllCourses = function() {
+    $('.course').show();
+    $('#menuItems *').removeClass('active');
+  }
+
+  /*
+    DOM events
+  */
+
+
+  /*
+    Initialization
+  */
+  populateCourses();
+
+});
